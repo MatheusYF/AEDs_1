fec0871265679b4c03d53a0af121765dfb960269
Matheus Arantes <mc2522351@gmail.com>
2023-09-15 20:40:49 -0300

    Q 1

--- code/noname.c
+++ code/noname.c
@@ -3,7 +3,12 @@
 
 int main() {
 
+  int n1, n2, n3;
+  printf("\nDigite 3 numeros\n");
+  scanf("%d %d %d", &n1, &n2, &n3);
   
+  int r = n1+n2+n3;
+  printf("\n%d + %d + %d = %d\n", n1, n2, n3, r);
 
   return 0;
 }
