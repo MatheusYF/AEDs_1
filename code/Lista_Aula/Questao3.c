7c1a31e80a517f35a85d6264be08584f80daa257
Matheus Arantes <mc2522351@gmail.com>
2023-09-27 11:34:32 -0300

    Update Aula

---
+++ code/Lista_Aula/Questao3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int soma(float som,float den) {
+
+  for(int rep = 1; rep < den; rep++) {
+
+    rep = rep+den;
+
+  }
+
+
+}
+
+int main() {
+  // Entrada de dados
+  float som;
+  float den;
+
+  printf("\nDigite um numero inteiro e positivo: ");
+  scanf("%f", &den);
+
+
+
+  return 0;
+}
